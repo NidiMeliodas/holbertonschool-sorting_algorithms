475dbc296fa2369d5ca9642b4920765b96c4caaa
mistWil <7448@holbertonstudents.com>
2024-01-15 12:48:47 +0100

    add betty-style

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers
+ * in ascending order using Bubble sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	int swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				/* Print array after each swap */
+				print_array(array, size);
+
+				swapped = 1;
+			}
+		}
+
+		/* If no two elements were swapped, the array is already sorted */
+		if (swapped == 0)
+			break;
+	}
+}
+
+
